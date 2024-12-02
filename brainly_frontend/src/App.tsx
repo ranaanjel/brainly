c7c6c9d125aa3550224ce8a180fe93630c41e1e1
ranaanjel <anjelranawork@gmail.com>
2024-12-02 21:28:58 +0530

    added the frontend boilerplate

---
+++ brainly_frontend/src/App.tsx
@@ -0,0 +1,12 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return <>
+    <div className='border-red-100 border-8 size-30'>
+    hello world
+    </div>
+  </>
+}
+
+export default App
